138bf41d2c1d801d0f8740cdcf0830675fe8b178
yujin <qrt365@naver.com>
2024-01-10 17:45:58 +0900

    EP_06 | DOM 추가&제거 2

--- lectures/2_dom-control/2_dynamic-element.js
+++ lectures/2_dom-control/2_dynamic-element.js
@@ -1,20 +1,43 @@
-let addBeforeBtn = document.getElementById('before');
 let addAfterBtn = document.getElementById('after');
+let addBeforeBtn = document.getElementById('before');
+let removeTargetBtn = document.querySelector('#target-remove');
+let removeBtn = document.querySelector('.remove-btn');
 let inputBox = document.querySelector('input');
 
 console.log(inputBox.value);
-
-addBeforeBtn.addEventListener('click', function () {
-  let ul = document.querySelector('ul');
-  let li = document.createElement('li');
-  let targetLi = document.querySelector('#target'); // 타겟 지정
-  li.textContent = inputBox.value;
-  ul.insertBefore(li, targetLi);
-});
 
 addAfterBtn.addEventListener('click', function () {
   let ul = document.querySelector('ul');
   let li = document.createElement('li');
   li.textContent = inputBox.value;
+
+  let button = document.createElement('button');
+  button.textContent = 'X';
+  // button.setAttribute('class', 'remove-btn');
+  button.classList.add('remove-btn');
+  button.addEventListener('click', removeParentNode);
+  li.appendChild(button);
   ul.appendChild(li);
+
+  inputBox.value = '';
+  inputBox.focus();
 });
+
+addBeforeBtn.addEventListener('click', function () {
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  let targetLi = document.querySelector('#target');
+  li.textContent = inputBox.value;
+  ul.insertBefore(li, targetLi);
+});
+
+removeTargetBtn.addEventListener('click', function () {
+  let targetLi = document.querySelector('#target');
+  targetLi.remove();
+});
+
+removeBtn.addEventListener('click', removeParentNode);
+
+function removeParentNode(event) {
+  event.target.parentNode.remove();
+}
