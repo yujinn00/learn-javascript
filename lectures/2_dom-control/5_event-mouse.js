99220db78d19361191f30495d06ab7f22e730b59
yujin <qrt365@naver.com>
2024-01-15 15:57:27 +0900

    EP_09 | 이벤트 다루기 2

--- lectures/2_dom-control/5_event-mouse.js
+++ lectures/2_dom-control/5_event-mouse.js
@@ -1,21 +1,35 @@
 let rectangle = document.querySelector('#rectangle');
+let body = document.querySelector('body');
 
 rectangle.addEventListener('mousedown', function (event) {
-  console.log('mousedown');
+  // console.log('mousedown');
+  // console.log(event.target);
+  console.log(event.target.getBoundingClientRect());
+  // console.log('clientX: ', event.clientX, 'clientY: ', event.clientY);
+  // console.log('pageX: ', event.pageX, 'pageY: ', event.pageY);
 });
 
-rectangle.addEventListener('mouseup', function (event) {
-  console.log('mouseup');
-});
+// body.addEventListener('click', function (event) {
+//   console.log('pageX: ', event.pageX, 'pageY: ', event.pageY);
+//   let div = document.createElement('div');
+//   div.classList.add('circle');
+//   div.style.left = event.pageX - 25 + 'px';
+//   div.style.top = event.pageY - 25 + 'px';
+//   body.appendChild(div);
+// });
 
-rectangle.addEventListener('mouseenter', function (event) {
-  console.log('mouseenter');
-});
+// rectangle.addEventListener('mouseup', function (event) {
+//   console.log('mouseup');
+// });
 
-rectangle.addEventListener('mouseleave', function (event) {
-  console.log('mouseleave');
-});
+// rectangle.addEventListener('mouseenter', function (event) {
+//   console.log('mouseenter');
+// });
 
-rectangle.addEventListener('mousemove', function (event) {
-  console.log('mousemove');
-});
+// rectangle.addEventListener('mouseleave', function (event) {
+//   console.log('mouseleave');
+// });
+
+// rectangle.addEventListener('mousemove', function (event) {
+//   console.log('mousemove');
+// });
