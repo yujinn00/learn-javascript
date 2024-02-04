46f6cc4a6d79757f32a376be172d1445dba3e373
yujin <qrt365@naver.com>
2024-02-04 19:04:28 +0900

    EP_22 | 브라우저 객체 모델

---
+++ lectures/4_else/24.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// // window 객체
+// window.open('https://google.com');
+// window.close();
+// window.alert('경고!!!');
+
+// // document 객체
+// document.querySelector('#custom-id');
+// document.querySelector('#custom-id').textContent = '원하는 텍스트';
+
+// // history 객체
+// history.back();
+// history.forward();
+
+// // location 객체
+// location.host;
+// location.href = 'https://google.com';
+
+// // screen 객체
+// console.log('hello');
+// console.dir(screen);
+
+// // navigator 객체
+// navigator.geolocation.getCurrentPosition(function (pos) {
+//   let crd = pos.coords;
+//   console.log('Your current position is:');
+//   console.log(`Latitude: ${crd.latitude}`);
+//   console.log(`Longitude: ${crd.longitude}`);
+//   console.log(`More or Less ${crd.accuracy} meters.`);
+// });
