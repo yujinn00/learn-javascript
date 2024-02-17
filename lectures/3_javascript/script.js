956a5197640627792fc918677a0fdb4e35432b96
yujin <qrt365@naver.com>
2024-02-17 17:15:06 +0900

    EP_24 | script defer, async

---
+++ lectures/3_javascript/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', function () {
+  alert('Hello World!');
+});
+
+// window.onload = function () {
+//   alert('onload');
+//   // let btn = document.querySelector('#btn');
+//   // btn.addEventListener('click', function () {
+//   //   alert('Hello World!');
+//   // });
+// };
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   alert('DOMContentLoaded');
+//   // let btn = document.querySelector('#btn');
+//   // btn.addEventListener('click', function () {
+//   //   alert('Hello World!');
+//   // });
+// });
